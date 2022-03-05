ebb725f98ee3dcf353f7efb8ec5f695d7ef794ed
Алексей Игонин <Igoni@DESKTOP-8OH60EI.(none)>
2022-03-05 18:08:48 +0300

    Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,24 +1,25 @@
 /* eslint-disable no-console */
 /* eslint-disable eqeqeq */
 function getRandomInt(min, max) {
-  const int = Math.floor(Math.random(min, max));
-  if (int >= min || int <= max) {
+  const random = Math.floor(min + Math.random() * (max + 1 - min));
+  if (random == 20) {
     console.log(true);
-  }else if (min > max) {
+  }else if (min >= max) {
     console.log(false);
   } else {
     console.log(false);
   }
 }
 
-getRandomInt(1, 5);
+getRandomInt(19, 22);
 
-function test (a, b = 100) {
-  if (a.length <= b) {
+function stringLength (text, peak = 40) {
+  const symbols = text.length;
+  if (symbols <= peak) {
     console.log(true);
   } else {
     console.log(false);
   }
 }
 
-test('Любая строка короче ста символов');
+stringLength('Любая строка короче ста символов');
