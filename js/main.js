97c808334eeb59c2a8b93516130a4c12b4c30b07
Алексей Игонин <Igoni@DESKTOP-8OH60EI.(none)>
2022-03-14 13:25:41 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -23,28 +23,25 @@ stringLength('Любая строка короче ста символов');
 // console.log (stringLength('Любая строка больше ста символов (Любая строка больше ста символов)'));
 
 
-const ID = [];
-const PHOTOS = [];
+let id;
 const NAMES = ['Артём', 'Дмитрий', 'Иван', 'Сергей', 'Алексей', 'Иван',];
 const DESCRIPTION = ['Звёздная ночь', 'Последний день Помпеи', 'Грачи прилетели', 'Мокрый луг', 'Девятый вал'];
 const MESSAGE = ['Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',];
 const getRandomPositiveInteger = (a, b) => {
-  const LOWER = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const UPPER = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-  const RESULT = Math.random() * (UPPER - LOWER + 1) + LOWER;
-  return Math.floor(RESULT);
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 };
 
 const generatePost = () => {
   const randomNameIndex = getRandomPositiveInteger(0, NAMES.length - 1);
   const randomDescriptionIndex = getRandomPositiveInteger(0, DESCRIPTION.length - 1);
-  const randomIdIndex = getRandomPositiveInteger(0, ID.length - 1);
-  const randomAvatarIndex = getRandomPositiveInteger(0, PHOTOS.length - 1);
   const randomMassageIndex = getRandomPositiveInteger(0, MESSAGE.length - 1);
 
   return {
-    id: ID[randomIdIndex],
-    url: `photos/${  PHOTOS[randomAvatarIndex]  }.jpg`,
+    id: id,
+    url: `photos/${  id  }.jpg`,
     description: DESCRIPTION[randomDescriptionIndex],
     names: NAMES[randomNameIndex],
     likes: getRandomPositiveInteger(15, 200),
@@ -56,8 +53,7 @@ const generatePost = () => {
 const creatingArrayOne = () => {
   const descriptionPhoto = [];
   for (let i = 0; i < 25; i++) {
-    ID[i] = i + 1;
-    PHOTOS[i] = i + 1;
+    id = i + 1;
     descriptionPhoto.push(generatePost());
   }
   return descriptionPhoto;
