78d409bc7be5bd14f14660cdc757215d01db4120
Алексей Игонин <Igoni@DESKTOP-8OH60EI.(none)>
2022-04-04 10:49:16 +0300

    Отрисуй меня полностью

--- js/main.js
+++ js/main.js
@@ -6,4 +6,5 @@ import {getRandomInt, stringLength} from './util.js';
 getRandomInt(0, 100);
 stringLength('Любая строка короче ста символов');
 
-import './picture.js';
+import {generatePicture} from './picture.js';
+generatePicture();
--- js/picture.js
+++ js/picture.js
@@ -1,17 +1,18 @@
 import { generatingArray } from './data.js';
-const pictures =document.querySelector('.pictures');
 
-const newElement = document.querySelector('#picture');
-const similarPictureElement = newElement.content.querySelector('.picture');
-const similarListFragment = document.createDocumentFragment();
-
-const exportObject = generatingArray();
-exportObject.forEach((elem) => {
-  const pictureElement = similarPictureElement.cloneNode(true);
-  pictureElement.querySelector('.picture__img').setAttribute('src', elem.url);
-  pictureElement.querySelector('.picture__comments').textContent = elem.comments.id;
-  pictureElement.querySelector('.picture__likes').textContent = elem.likes;
-  similarListFragment.appendChild(pictureElement);
-});
-
-pictures.appendChild(similarListFragment);
+export const generatePicture = () => {
+  const pictures =document.querySelector('.pictures');
+  const newElement = document.querySelector('#picture');
+  const similarPictureElement = newElement.content.querySelector('.picture');
+  const similarListFragment = document.createDocumentFragment();
+  const exportObject = generatingArray();
+  exportObject.forEach((elem) => {
+    const pictureElement = similarPictureElement.cloneNode(true);
+    pictureElement.querySelector('.picture__img').setAttribute('src', elem.url);
+    pictureElement.querySelector('.picture__comments').textContent = elem.comments.id;
+    pictureElement.querySelector('.picture__likes').textContent = elem.likes;
+    similarListFragment.appendChild(pictureElement);
+  });
+  pictures.appendChild(similarListFragment);
+};
+generatePicture();
